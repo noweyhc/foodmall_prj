5e2342b6b8fb3ee6c6fe9b218123e413b0cd7530
장일규 <69107255+Jangilkyu@users.noreply.github.com>
2021-04-06 10:23:24 +0900

    [Fix] LoginMvcConfig

--- mall/src/main/java/com/mall/config/LoginMvcConfig.java
+++ mall/src/main/java/com/mall/config/LoginMvcConfig.java
@@ -12,7 +12,7 @@ public class LoginMvcConfig implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		
-		 registry.addInterceptor(new LoginSessionListener()) .addPathPatterns("/mypage/*") .addPathPatterns("/inquiry/*") .addPathPatterns("/payment/*");
+		 registry.addInterceptor(new LoginSessionListener()) .addPathPatterns("/mypage/*") .addPathPatterns("/inquiry/*") .addPathPatterns("/login/*").addPathPatterns("/payment/*").addPathPatterns("/cart/*");
 	}//addInterceptors
 	
 }//class
